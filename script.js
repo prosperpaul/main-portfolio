246bd75d9772a17021967b10780dbcb8de8a806d
paul <amakapaul4cbn@gmail.com>
2025-11-07 23:32:15 -1200

    feat: chaged projects, and added stylings.

--- script.js
+++ script.js
@@ -10,49 +10,59 @@ let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
+  let top = window.scrollY;
+
   sections.forEach((sec) => {
-    let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach.apply((links) => {
-        links.classList.remove("active");
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
         document
-          .querySelector("header nav a[href*=' + id + ']")
+          .querySelector(`header nav a[href*=${id}]`)
           .classList.add("active");
       });
     }
   });
 
+  // Sticky header
   let header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 100);
 
+  // Remove toggle menu when scrolling
   menuIcon.classList.remove("fa-xmark");
   navbar.classList.remove("active");
 };
+
+// Scroll reveal animations
 ScrollReveal({
   distance: "80px",
   duration: 2000,
-  display: 200,
+  delay: 200,
 });
 
-ScrollReveal().reveal(".home-content, heading", { origin: "top" });
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
 ScrollReveal().reveal(
-  ".home-img, .services-container, .portfolio-box .contact form",
+  ".home-img, .services-container, .portfolio-box, .contact form",
   { origin: "bottom" }
 );
-ScrollReveal(".home-contact h1, .about-img", { origin: "left" });
-ScrollReveal(".home-contact p, .about-content", { origin: "right" });
+ScrollReveal().reveal(".home-contact h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-contact p, .about-content", { origin: "right" });
 
+// Typing text animation
 const typed = new Typed(".multiple-text", {
+
   strings: [
     "Web Developer",
     "Graphic Designer",
     "Entrepreneur",
     "Biotechnologist",
   ],
+
+  strings: ["Front-End Developer", "Climate Advocate", "Graphic Designer"],
+
   typeSpeed: 70,
   backSpeed: 70,
   backDelay: 1000,
